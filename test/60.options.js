36364c3c4ab1d4433013d2ae0971b365c3173beb
Yusuke Kawasaki <u-suke@kawa.net>
2018-04-26 10:01:01 +0900

    tag option to change delimiter string

--- test/60.options.js
+++ test/60.options.js
@@ -9,9 +9,18 @@ var TITLE = __filename.replace(/^.*\//, "");
 
 describe(TITLE, function() {
   var sample1 = fs.readFileSync(__dirname + "/sample/sample1.html");
+  var sample2 = fs.readFileSync(__dirname + "/sample/sample2.html");
 
   it("trim", function() {
     var render = compile(sample1, {trim: true});
+    assert.equal(render({list: [{name: "foo"}, {name: "bar"}]}), '<ul>\n  <li>foo</li>\n  <li>bar</li>\n</ul>');
+    assert.equal(render({list: {name: "foo"}}), '<ul>\n  <li>foo</li>\n</ul>');
+    assert.equal(render({list: true, name: "foo"}), '<ul>\n  <li>foo</li>\n</ul>');
+    assert.equal(render(), '<ul>\n</ul>');
+  });
+
+  it("tag", function() {
+    var render = compile(sample2, {tag: "<% %>", trim: true});
     assert.equal(render({list: [{name: "foo"}, {name: "bar"}]}), '<ul>\n  <li>foo</li>\n  <li>bar</li>\n</ul>');
     assert.equal(render({list: {name: "foo"}}), '<ul>\n  <li>foo</li>\n</ul>');
     assert.equal(render({list: true, name: "foo"}), '<ul>\n  <li>foo</li>\n</ul>');
