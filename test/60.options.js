2aec76ec1ba5e88ad1e370d61543f56276cc9fde
Yusuke Kawasaki <u-suke@kawa.net>
2018-04-26 10:51:04 +0900

    tag switch: {{= <% %> =}}

--- test/60.options.js
+++ test/60.options.js
@@ -11,19 +11,51 @@ describe(TITLE, function() {
   var sample1 = fs.readFileSync(__dirname + "/sample/sample1.html");
   var sample2 = fs.readFileSync(__dirname + "/sample/sample2.html");
 
-  it("trim", function() {
+  var context1 = {list: [{name: "foo"}, {name: "bar"}]};
+  var context2 = {list: {name: "foo"}};
+  var context3 = {list: true, name: "foo"};
+
+  var expect1 = '<ul>\n  <li>foo</li>\n  <li>bar</li>\n</ul>';
+  var expect2 = '<ul>\n  <li>foo</li>\n</ul>';
+  var expect3 = '<ul>\n</ul>';
+
+  it("{trim: true}", function() {
     var render = compile(sample1, {trim: true});
-    assert.equal(render({list: [{name: "foo"}, {name: "bar"}]}), '<ul>\n  <li>foo</li>\n  <li>bar</li>\n</ul>');
-    assert.equal(render({list: {name: "foo"}}), '<ul>\n  <li>foo</li>\n</ul>');
-    assert.equal(render({list: true, name: "foo"}), '<ul>\n  <li>foo</li>\n</ul>');
-    assert.equal(render(), '<ul>\n</ul>');
+    assert.equal(render(context1), expect1);
+    assert.equal(render(context2), expect2);
+    assert.equal(render(context3), expect2);
+    assert.equal(render(), expect3);
   });
 
-  it("tag", function() {
+  it('{tag: "<% %>"}', function() {
     var render = compile(sample2, {tag: "<% %>", trim: true});
-    assert.equal(render({list: [{name: "foo"}, {name: "bar"}]}), '<ul>\n  <li>foo</li>\n  <li>bar</li>\n</ul>');
-    assert.equal(render({list: {name: "foo"}}), '<ul>\n  <li>foo</li>\n</ul>');
-    assert.equal(render({list: true, name: "foo"}), '<ul>\n  <li>foo</li>\n</ul>');
-    assert.equal(render(), '<ul>\n</ul>');
+    assert.equal(render(context1), expect1);
+    assert.equal(render(context2), expect2);
+    assert.equal(render(context3), expect2);
+    assert.equal(render(), expect3);
+  });
+
+  it("{{= <% %> =}} new tag", function() {
+    var render = compile("{{= <% %> =}}" + sample2, {trim: true});
+    assert.equal(render(context1), expect1);
+    assert.equal(render(context2), expect2);
+    assert.equal(render(context3), expect2);
+    assert.equal(render(), expect3);
+  });
+
+  it("old tag {{= <% %> =}} new tag", function() {
+    var render = compile(sample1 + "{{= <% %> =}}" + sample2, {trim: true});
+    assert.equal(render(context1), expect1 + expect1);
+    assert.equal(render(context2), expect2 + expect2);
+    assert.equal(render(context3), expect2 + expect2);
+    assert.equal(render(), expect3 + expect3);
+  });
+
+  it("new tag {{= <% %> =}} old tag", function() {
+    var render = compile(sample2 + "{{= <% %> =}}" + sample1);
+    assert.equal(render(context1), sample2 + sample1);
+    assert.equal(render(context2), sample2 + sample1);
+    assert.equal(render(context3), sample2 + sample1);
+    assert.equal(render(), sample2 + sample1);
   });
 });
